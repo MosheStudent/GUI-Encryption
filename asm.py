116cc5d4f63b1f23bfed64f24994a107088ce067
Moshe Bekritsky <Moshebekritsky@gmail.com>
2024-12-05 19:59:39 +0200

    added initial gui

--- asm.py
+++ asm.py
@@ -1,92 +1,141 @@
 import random
+
 from sympy import mod_inverse
 
+
 class RSA:
+
     def __init__(self):
+
         self.primes = []
+
 
         #private data:
         self.q = 0
         self.p = 0
-        self.totient = (self.q-1)*(self.p-1)
+
 
         #Public data:
         self.e = 0
-        self.publicKey = 0
+        self.n = 0
+        self.PUBLIC_KEY = []
 
         #initialzing functions
         self.primeGen()
         self.generateE()
 
     #function returns greatest common divisor of 2 numbers (recursive)
+
     def gcd(self, max, min):
+
         if (max % min == 0):
+
             return min
 
+
         r = max % min
+
         max = min
         min = r
+
 
         return self.gcd(max, min)
         
         
 
+
     #function to check if prime number
+
     def isPrime(self, num):
+
         for i in range(2, num-1):
+
             if (num % i == 0):
+
                 return False
+
 
         return True
 
+
     #generates and fills the list with a range of prime numbers
+
     """The range is relativaly small for encryption, but this
+
     is a simple school project, 1000 will suffice"""
 
+
     def primeGen(self):
+
         randStart  = random.randint(2, 1000)
+
         randEnd = random.randint(randStart, randStart + 1000)
 
+
         for i in range(randStart, randEnd):
+
             if (self.isPrime(i)):
                 self.primes.append(i)
 
-        self.q = random.choice(self.primes)
-        self.p = random.choice(self.primes)
-        self.publicKey = self.p * self.q
 
+        self.q = random.choice(self.primes)
+
+        self.p = random.choice(self.primes)
+        self.n = self.p * self.q
+        self.PUBLIC_KEY.append(self.n)
+
+    #Generates the encryption exponent (public)
     def generateE(self):
         comp = (self.p - 1) * (self.q - 1)
 
+
         for i in range (2, comp-1):
+
             if (self.gcd(comp, i) == 1):
                 self.e = i
+                self.PUBLIC_KEY.append(self.e)
                 return
 
+    #Encodes given text and returns
     def enc(self, text):
+
         encText = ""
 
+
         for char in text:
-            encText += str((ord(char) ** self.e) % self.publicKey) + "|"
+
+            encText += str((ord(char) ** self.e) % self.n) + "|"
+
 
         return encText
         
         
 
+    #decodes given text with q and p values, and returns
     def dec(self, text, q, p):
+
         DefChar = ''
+
         decString = ""
+
 
         d = mod_inverse(self.e, ((q-1) * (p-1)))
 
 
+
         for char in text:
+
             if (char == '|'):
-                decString += chr((int(DefChar) ** d ) % self.publicKey) 
+
+                decString += chr((int(DefChar) ** d ) % self.n) 
+
                 DefChar = ''
 
+
             else:
+
                 DefChar += str(char)
+
 
         return decString
                 
@@ -94,15 +143,5 @@ class RSA:
 
                 
 
-
-
-r = RSA()
-
-print(1)
-t = r.enc("Hello my little nigger")
-print(t)
-f = r.dec(t, r.q, r.p)
-
-print(f)
 
 
--- main.py
+++ main.py
@@ -1,72 +1,56 @@
 import tkinter
+from tkinter import filedialog
+
 import sym
+import asm
+import fileSys
 
-#sym class init
-cipher = sym.Sym()
 
-#calls to sym class
-def enc(msg):
-    textval.set(f'{cipher.cipher(msg)} \t key={cipher.key}')
 
-def dec(msg):
-    text3 = tkinter.Label(root, text="Enter key: ")
-    text3.pack()
+class MainWIn:
+    def __init__(self):
+        self.root = tkinter.Tk()
+        self.encTypes = ["CEASER", "RSA"]
+        
+        #window Setup Funcs
+        self.winSetup()
+        self.menuSetup()
+        self.encryptionType()
+
+        self.root.mainloop()
+
+    def encryptionType(self):
+        #asm or sym
+        optionVar = tkinter.StringVar(self.root)
+        optionVar.set(self.encTypes[0])
+        dropdown = tkinter.OptionMenu(self.root, optionVar, *self.encTypes)
+        dropdown.pack()
+        
+
+    def winSetup(self):
+        #Basic window init:
+        self.root.geometry("400x400")
+        self.root.title("Encryption")
+
+    def menuSetup(self):
+        #menu:
+        menu = tkinter.Menu(self.root)
+        self.root.config(menu=menu)
+        filemenu = tkinter.Menu(menu)
+
+        menu.add_cascade(label='File', menu=filemenu)
+        filemenu.add_command(label='Import', command=self.browseFiles)
+
+    #Open file explorer for file import
+    def browseFiles(self):
+        filename = tkinter.filedialog.askopenfilename(initialdir = "/",
+        title = "Select a File",
+        filetypes = (("Text files","*.txt*"),("all files", "*.*")))
+
+
+r = MainWIn()
+
+
+
 
     
-    inputLine = tkinter.Entry(root)
-    inputLine.pack()
-
-    
-    def write():
-        textval.set(cipher.dec(msg, inputLine.get()))
-        button2.destroy()
-        inputLine.destroy()
-        text3.destroy()
-
-
-
-    button2 = tkinter.Button(root, text="Enter", command=write)
-    button2.pack()
-
-
-#window startup
-root = tkinter.Tk()
-root.geometry("400x400")
-header = root.title("Cipher Tool")
-
-#encryption or decryption
-choices = ["Encrypt", "Decrypt"]
-initVal = tkinter.StringVar(root)
-initVal.set(choices[0])
-choiceBox = tkinter.OptionMenu(root, initVal, *choices)
-choiceBox.pack(pady= 20)
-
-#text label
-text1 = tkinter.Label(root, text="Enter message:")
-text1.pack(pady=25)
-
-#Input line
-entryLine = tkinter.Entry(root)
-entryLine.pack(padx=40)
-
-#output label
-textval = tkinter.StringVar(root)
-outputLine = tkinter.Label(root, textvariable=textval)
-outputLine.pack(pady=20)
-
-#directs to decypher or to encrypt
-def commandCenter():
-    if (initVal.get() == choices[0]):
-        enc(entryLine.get())
-
-    else:
-        dec(entryLine.get())
-
-#inputButton
-button = tkinter.Button(text="Enter", command=commandCenter)
-button.pack()
-
-
-root.mainloop()
-
-#if __name__ == "main"
